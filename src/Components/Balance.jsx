48786660dee06bd71e02e54203221274f0cef2cb
vatsal2110 <49248727+vatsal2110@users.noreply.github.com>
2021-06-01 16:32:17 +0530

    Update Balance.jsx

--- src/Components/Balance.jsx
+++ src/Components/Balance.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React from "react";
+import Graph from "./Graph";
 
-function Balance()
-{
-    return (<div class="balance">
-    <div class="balanceheader"><p class="balanceheading">Balance</p><p class="balanceduration">Monthly<img className="balancecon" src="https://img-premium.flaticon.com/png/512/60/60781.png?token=exp=1622316944~hmac=753dadcc6548b4e2cb53ebddc8be5001"></img></p></div>
-    <hr></hr>
-    <div class="balanceitems">
-        <div class="bitem1">
-        <p class="bitemheading">Earnings</p>
-        <p class="stats">43.41%<span class="bitembutton"> +2.5% </span></p>
+function Balance() {
+  return (
+    <div className="balance">
+      <div className="balanceheader">
+        <p className="balanceheading">Balance</p>
+        <p className="balanceduration">
+          Monthly
+          <img
+            className="balancecon"
+            src="https://lh3.googleusercontent.com/proxy/Eg7d9VZvgFpoyOum1NU6Nh3wkcFbYq1odBs4jo5iOYQRVNvU_aro1xMjBTSJN3wbFx8yggODiRxhGnwzUJLq9ndbwvHEoKn5_nBZbU3klj0"
+          ></img>
+        </p>
+      </div>
+      <hr></hr>
+      <div className="balanceitems">
+        <div className="bitem1">
+          <p className="bitemheading">Earnings</p>
+          <p className="stats">
+            43.41%<span class="bitembutton"> +2.5% </span>
+          </p>
         </div>
-        <div class="bitem2">
-        <p class="bitemheading">Sales Value</p>
-        <p class="stats">$95,422<span class="bitembutton"> +13.5% </span></p>
+        <div className="bitem2">
+          <p className="bitemheading">Sales Value</p>
+          <p className="stats">
+            $95,422<span class="bitembutton"> +13.5% </span>
+          </p>
         </div>
+      </div>
+      <Graph />
     </div>
-    <img src="https://vistapointe.net/images/graph-wallpaper-16.jpg" class="graph"></img>
-    </div>);
+  );
 }
 
 export default Balance;
