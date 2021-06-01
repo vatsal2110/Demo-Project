7b1876e122d6f73f836a49512908141e7f40ef9a
vatsal2110 <49248727+vatsal2110@users.noreply.github.com>
2021-06-01 16:32:51 +0530

    Update NewUsers.jsx

--- src/Components/NewUsers.jsx
+++ src/Components/NewUsers.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
+import Chart from './Chart';
 
 function NewUsers()
 {
     return (<div class="newusers">
     <p class="nuheading">New Users</p>
     <p class="nuamount">94.2%<span class="nubutton"> +6.9% </span></p>
-    <div class="histogram">
-      <div class="hlevel"><div class="ulevel1"></div></div>
-      <div class="hlevel"><div class="ulevel2"></div></div>
-      <div class="hlevel"><div class="ulevel3"></div></div>
-      <div class="hlevel"><div class="ulevel4"></div></div>
-      <div class="hlevel"><div class="ulevel5"></div></div>
-      <div class="hlevel"><div class="ulevel6"></div></div>
-      <div class="hlevel"><div class="ulevel7"></div></div>
-      <div class="hlevel"><div class="ulevel8"></div></div>
-      <div class="hlevel"><div class="ulevel9"></div></div>
-    </div>
+    <Chart />
     </div>);
 }
 
